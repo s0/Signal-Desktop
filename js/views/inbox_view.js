79bfd53026ee6dc833d1c6d22e1f9b98298d88ea
lilia <liliakai@gmail.com>
2015-12-01 16:13:58 -0800

    Clean up open-event logic with new convos and search results
    Search view triggers an open event when a valid phone number is entered
    and the 'Create new contact' card is clicked.
    
    Inbox view should listen and respond to this event. It should also
    disregard select events on the new contact element since those are fired
    before phone number validation.
    
    Finally, the search view can stop listening to select events because the
    inbox view is already doing so.
    
    // FREEBIE

--- js/views/inbox_view.js
+++ js/views/inbox_view.js
@@ -101,6 +101,9 @@
                     this.searchView.$el.show();
                     this.inboxListView.$el.hide();
                 });
+                this.listenTo(this.searchView, 'open',
+                    this.openConversation.bind(this, null));
+
 
                 if (inboxCollection.length === 0) {
                     this.searchView.showAllContacts = true;
@@ -123,7 +126,7 @@
                 'click .hamburger': 'toggleMenu',
                 'click .show-debug-log': 'showDebugLog',
                 'click .show-new-conversation': 'showCompose',
-                'select .gutter .contact': 'openConversation',
+                'select .gutter .conversation-list-item': 'openConversation',
                 'input input.search': 'filterContacts'
             },
             filterContacts: function(e) {
